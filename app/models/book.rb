022822825df916cf7810e1396a4ae6ce00988b1d
satoyuki <yuki01377@gmail.com>
2021-01-06 22:30:10 +0900

    model add

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,40 @@
 class Book < ActiveRecord::Base
+  enum status: %w(reservation now_on_sale end_of_print)
+
   # scopeを定義
   scope :costly, -> {where("price > ?", 3000)}
   scope :written_about, -> (theme){where("name like ?", "%#{theme}%")}
   # default_scope ->{order("published_on asc")}
 
   belongs_to :publisher
-  
+
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: {maximum: 15}
+  validates :price, numericality: {greater_than_or_equal_to: 0}
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+
+  before_validation do |book|
+    book.name = book.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+
+  after_destroy do |book|
+    Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
+  end
+
+  def high_price?
+    price >= 5000
+  end
+  after_destroy :if => :high_price? do |book|
+    Rails.logger.warn "Book with high price is deleted: #{book.attributes.inspect}"
+    Rails.logger.warn "Please check!!"
+  end
 end
