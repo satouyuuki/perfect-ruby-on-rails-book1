5fdf3ecc13e21037716f9d2b00f13294c6fc7202
satoyuki <yuki01377@gmail.com>
2021-01-11 18:28:40 +0900

    イベント参加,キャンセル

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,8 @@ class EventsController < ApplicationController
   before_action :authenticate, except: :show
   def show
     @event = Event.find(params[:id])
+    @ticket = current_user && current_user.tickets.find_by(event_id: params[:id])
+    @tickets = @event.tickets.includes(:user).order(:created_at)
   end
 
   def new
